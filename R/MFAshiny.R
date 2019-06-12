9cf94c0ff3f5a5539a79e1401f7910071e2ddd61
Husson <husson@agrocampus-ouest.fr>
2019-06-12 14:10:02 +0200

    MFA

--- R/MFAshiny.R
+++ R/MFAshiny.R
@@ -1,7 +1,8 @@
+utils::globalVariables(c("objMFAshiny","myListOfThingsMFAshiny"))
 MFAshiny <-
 function(X){
-#    gassign("x", X)
     G <- .GlobalEnv
+    assign("objMFAshiny",ls(all.names=TRUE, envir=G),envir=G)
     assign("x", X, envir=G)
 
     if (!(inherits(X, "MFA") | inherits(X, "data.frame") | inherits(X, "matrix") | inherits(X, "MFAshiny"))){
@@ -10,15 +11,17 @@ function(X){
     if (is.matrix(X)==TRUE) 	X <- as.data.frame(X)
     if(is.data.frame(X)==TRUE){
       assign("nomData",sys.calls()[[1]][2], envir=G)
-      if(dim(X)[2]<=2)
-        stop(gettext('not enough variables in your dataset'))
-    outShiny=shiny::runApp(system.file("FactoMFAapp", package="Factoshiny"),launch.browser = TRUE)
+      if(ncol(X)<=2) stop(gettext('not enough variables in your dataset'))
+    outShiny <- shiny::runApp(system.file("FactoMFAapp", package="Factoshiny"),launch.browser = TRUE)
 #    outShiny <- shiny::runApp('C:/Users/husson/AOBox/Travail/huss/Divers/Site_Github/Factoshiny/inst/FactoMFAapp')
     }   else{
       assign("nomData",as.character(X$call$call)[2], envir=G)
-    outShiny=shiny::runApp(system.file("FactoMFAapp2", package="Factoshiny"),launch.browser = TRUE)
+    outShiny <- shiny::runApp(system.file("FactoMFAapp2", package="Factoshiny"),launch.browser = TRUE)
 #    outShiny <- shiny::runApp('C:/Users/husson/AOBox/Travail/huss/Divers/Site_Github/Factoshiny/inst/FactoMFAapp2')
     }
+  assign("myListOfThingsMFAshiny",setdiff(ls(all.names=TRUE,envir=G),c("outShiny",objMFAshiny)),envir=G)  ## on met "outShiny" pour ne pas le supprimer
+  rm(list=myListOfThingsMFAshiny, envir=G)
+  rm(list=c("myListOfThingsMFAshiny"),envir=G)
   if (outShiny$hcpcparam==TRUE) {
     resHCPC <- HCPCshiny(outShiny)
     print(list(invisible(outShiny),resHCPC))
